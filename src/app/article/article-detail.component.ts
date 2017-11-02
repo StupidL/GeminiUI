272008875eab4e887f8942723ba3546d09eb4aa7
Allen <562117676@qq.com>
2017-11-02 22:29:19 +0800

    Update article component

--- src/app/article/article-detail.component.ts
+++ src/app/article/article-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {ArticleDetailService} from './article-detail.service';
+import {Article} from './article';
 
 @Component({
   selector: 'article-detail',
@@ -9,15 +10,18 @@ import {ArticleDetailService} from './article-detail.service';
 
 
 export class ArticleDetailComponent {
+
+  article: Article;
+
   constructor(private articleDetailService: ArticleDetailService) {
 
   }
 
   showArticleDetail(): void {
-
+    this.article = this.articleDetailService.getArticleDetail();
   }
 
   hideArticleDetail(): void {
-
+    this.article = null;
   }
 }
--- src/app/article/article-detail.service.ts
+++ src/app/article/article-detail.service.ts
@@ -1,4 +1,15 @@
+import {Article} from './article';
+
+const MOCK_ARTICLE = {
+  id: 1234567,
+  title: 'Mock Article',
+  author: 'Allen',
+  time: '2017-11-02',
+  content: 'Hello everyone! This is a mock article',
+}
+
 export class ArticleDetailService {
-  getArticleDetail(): void {
+  getArticleDetail(): Article {
+    return MOCK_ARTICLE;
   }
 }
