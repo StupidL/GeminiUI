c366a937512939dbb9e95cc690cf296be1987195
Allen <562117676@qq.com>
2017-11-02 22:29:37 +0800

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {ArticleDetailComponent} from './article/article-detail.component';
+import {RoutingModule} from '../routing/routing.module';
+import {AboutComponent} from './about/about.component';
+import {AboutService} from "./about/about.service";
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     ArticleDetailComponent
   ],
   imports: [
-    BrowserModule
+    RoutingModule,
+    BrowserModule,
   ],
-  providers: [],
+  providers: [AboutService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
